6db2937062dd18e6b7eafae5c80717edff373cfe
Toby <newtobycm@gmail.com>
2023-07-17 19:29:11 -0700

    user can choose their own room

--- server/main.ts
+++ server/main.ts
@@ -1,28 +1,13 @@
-function randomString(length: number): string {
-  const chars =
-    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return result;
-}
-
 Bun.serve({
   fetch(req, server) {
     const url = new URL(req.url);
-
-    if (url.pathname === "/new") {
-      return new Response(randomString(6), {
-        headers: { "Access-Control-Allow-Origin": "*" },
-      });
-    }
 
     const roomId = url.pathname.split("/")[1];
 
     if (!server.upgrade(req, { data: { roomId } })) {
       return new Response(null, { status: 404 });
     }
+
     return new Response(null, { status: 101 });
   },
   websocket: {
